eb5902176c4f7b4fdb83c7498c2f28dbbd531fcd
jainesh patel <pateljainesh1708@gmail.com>
2022-07-21 15:52:05 +0530

    orignalinternship app-

--- app/src/main/java/com/example/orignalinternship/constanturl.java
+++ app/src/main/java/com/example/orignalinternship/constanturl.java
@@ -1,7 +1,7 @@
 package com.example.orignalinternship;
 
 public class constanturl {
-    public static final String BASE_URL = "http://192.168.43.198/internship15days/";
+    public static final String BASE_URL = "http://jaineshpatel.000webhostapp.com/internship15days/";
     public static final String SIGNUP_URL = BASE_URL + "signup_android.php";
     public static final String LOGIN_URL = BASE_URL + "login.php";
     public static final String CATEGORY_URL = BASE_URL + "getcategory.php";
